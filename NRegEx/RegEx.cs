ac0864d528f85f1a2f9ddd55e0499f5994b0f3b4
yyl20050115 <yyl_20050115@hotmail.com>
2023-03-02 23:52:21 +0800

    fx

--- NRegEx/RegEx.cs
+++ NRegEx/RegEx.cs
@@ -491,26 +491,5 @@ public class Regex
         return result.ToArray();
     }
     private static int FixDirection(int direction) => direction >= 0 ? 1 : -1;
-
-
-    public static string DoReplace(string input, string pattern, string replacement)
-    {
-        var regex = new Regex(pattern);
-        var matches = regex.Matches(input);
-        return DoReplace(input, matches, replacement);
-    }
-    public static string DoReplace(string input, List<Match> matches, string replacement)
-    {
-        var builder = new StringBuilder();
-        var parser = new RegExReplacementParser(replacement);
-        var list = parser.Parse();
-        foreach(var match in matches)
-        {
-            foreach (var repl in list)
-            {
-                builder.Append(repl.Replace(input, match));
-            }
-        }
-        return builder.ToString();
-    }
+    
 }
--- NRegEx/RegExTools.cs
+++ NRegEx/RegExTools.cs
@@ -1,8 +1,26 @@
-﻿namespace NRegEx;
+﻿using System.Text;
+
+namespace NRegEx;
 
 public static class RegExTools
 {
-    //CHECK
-    public static string Result(this Match match, string replacement) 
-        => match.Source.ReplaceAll(match.Input, replacement, new List<Match> { match });
+    public static string Result(this Match match, string replacement)
+        => DoReplace(match.Input, match, replacement);
+    public static string DoReplace(string input, string pattern, string replacement)
+    {
+        var regex = new Regex(pattern);
+        var match = regex.Match(input);
+        return DoReplace(input, match, replacement);
+    }
+    public static string DoReplace(string input, Match match, string replacement)
+    {
+        var builder = new StringBuilder();
+        var parser = new RegExReplacementParser(replacement);
+        var list = parser.Parse();
+        foreach (var repl in list)
+        {
+            builder.Append(repl.Replace(input, match));
+        }
+        return builder.ToString();
+    }
 }
