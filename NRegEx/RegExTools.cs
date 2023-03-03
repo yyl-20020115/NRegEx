18c2b4e051d86e2dbee4dd59bc74c34500ac95bf
yyl20050115 <yyl_20050115@hotmail.com>
2023-03-03 12:54:19 +0800

    fx

--- NRegEx/RegExTools.cs
+++ NRegEx/RegExTools.cs
@@ -4,10 +4,14 @@ namespace NRegEx;
 
 public static class RegExTools
 {
-    public static string DoReplace(string input, string pattern, string replacement)
+    public static string Result(this Match match, string replacement)
+        => DoReplace(match.Input, new Match[] { match }, replacement);
+    public static string Result(this IEnumerable<Match> matches, string replacement)
+        => DoReplace(matches.First().Input, matches.ToArray(), replacement);
+    public static string DoReplace(string input, string pattern, string replacement) 
+        => DoReplace(input, new Regex(pattern).Matches(input).ToArray(), replacement);
+    public static string DoReplace(string input, Match[] matches, string replacement)
     {
-        var regex = new Regex(pattern);
-        var matches = regex.Matches(input);
         var builder = new StringBuilder();
         var parser = new RegExReplacementParser(replacement);
         var list = parser.Parse();
@@ -30,18 +34,18 @@ public static class RegExTools
         return builder.ToString();
     }
 
-    public static List<(Match? match,string input)> SegmentInput(string input,List<Match> matches)
+    public static List<(Match? match,string input)> SegmentInput(string input,params Match[] matches)
     {
         var segments = new List<(Match? match, string input)>();
 
-        if (matches.Count == 0)
+        if (matches.Length == 0)
         {
             segments.Add((null, input));
         }
         else
         {
             var last = 0;
-            for (int i = 0; i < matches.Count; i++)
+            for (int i = 0; i < matches.Length; i++)
             {
                 if (matches[i].InclusiveStart > last)
                 {
